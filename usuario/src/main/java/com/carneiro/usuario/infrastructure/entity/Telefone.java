93d104835ee97e206e0e3e8c650d366412227887
Gabriel <gabrielcarneirodesouza13@gmail.com>
2026-04-08 18:21:27 -0300

    adicionadas classes de security

---
+++ usuario/src/main/java/com/carneiro/usuario/infrastructure/entity/Telefone.java
@@ -0,0 +1,25 @@
+package com.carneiro.usuario.infrastructure.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "telefone")
+
+public class Telefone {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "numero", length = 11)
+    private String numeroTelefone;
+    @Column(name = "ddd", length = 3)
+    private String ddd;
+}
